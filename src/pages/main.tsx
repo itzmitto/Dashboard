351bc3c1d2948340cd187945b0907e41cd063e57
Andre <andre.babirian2004@gmail.com>
2026-04-21 19:59:53 +0200

    3de commit

---
+++ src/pages/main.tsx
@@ -0,0 +1,12 @@
+import '../css/main.css'
+
+function Main() { 
+  return (
+    <div className="main-page">
+      <h1>Dashboard</h1>
+      <p>Welkom op je mijn school dashboard</p>
+    </div>
+  )
+}
+
+export default Main
