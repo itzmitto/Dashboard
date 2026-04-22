7e0da13f031d614adbee0a8a066c0a7e9216f84c
Andre <andre.babirian2004@gmail.com>
2026-04-22 21:46:17 +0200

    6de commit begonnen met de sidebar

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,23 @@
-import '../css/main.css'
+import '../styling/main.css'
 
 function Main() { 
   return (
-    <div className="main-page">
-      <h1>Dashboard</h1>
-      <p>Welkom op je mijn school dashboard</p>
+    <div className="layout">
+      <div className="sidebar">
+        <h2>Menu</h2>
+        <ul>
+          <li>Dashboard</li>
+          <li>Profiel</li>
+          <li>Instellingen</li>
+        </ul>
+      </div>
+
+      <div className="main-page">
+        <h1>Dashboard</h1>
+        <p>Welkom op je mijn school dashboard</p>
+      </div>
     </div>
-  )
+  )   
 }
 
-export default Main
+export default Main 
