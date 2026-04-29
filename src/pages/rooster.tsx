ddcbe3955efdead051c510fdd9e350efb6307e7a
Andre <andre.babirian2004@gmail.com>
2026-04-29 23:27:13 +0200

    26

--- src/pages/rooster.tsx
+++ src/pages/rooster.tsx
@@ -13,35 +13,35 @@ function Rooster() {
           <div className="card">
             <h1>Maandag</h1>
             <h2>08:30 tot 17:00</h2>
-            <h3>lokaal Groen 2073</h3>
+            <h3>lokaal Groen 2.073</h3>
             <h4>Vak: Flex</h4>
           </div>
 
           <div className="card">
             <h1>Dinsdag</h1>
             <h2>08:30 tot 17:00</h2>
-            <h3>lokaal Groen 2073</h3>
+            <h3>lokaal Groen 2.073</h3>
             <h4>Vak: Flex</h4>
           </div>
 
           <div className="card">
             <h1>Woensdag</h1>
             <h2>08:30 tot 17:00</h2>
-            <h3>lokaal Groen 2073</h3>
+            <h3>lokaal Groen 2.073</h3>
             <h4>Vak: Flex en Rekenen</h4>
           </div>
 
           <div className="card">
             <h1>Donderdag</h1>
             <h2>08:30 tot 17:00</h2>
-            <h3>lokaal Groen 2073</h3>
+            <h3>lokaal Groen 2.073</h3>
             <h4>Vak: Flex, Nederlands en Burgerschap</h4>
           </div>
 
           <div className="card">
             <h1>Vrijdag</h1>
             <h2>08:30 tot 13:30</h2>
-            <h3>lokaal Groen 2073</h3>
+            <h3>lokaal Groen 2.073</h3>
             <h4>Vak: Flex</h4>
           </div>
         </div>
