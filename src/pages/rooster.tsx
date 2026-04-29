a3b56efc5bbffd78bc82bc803c9636628dfdd806
Andre <andre.babirian2004@gmail.com>
2026-04-29 23:18:34 +0200

    21

--- src/pages/rooster.tsx
+++ src/pages/rooster.tsx
@@ -8,36 +8,46 @@ function Rooster() {
           <div className="titleBlock">
             <h1>School Rooster</h1>
             <h2>Dit is mijn schoolrooster voor de week</h2>
-
           </div>
 
           <div className="cardsContainer">
             <div className="card main">
-              <h1>Portfolio website</h1>
-              <h2>Webapp</h2>
-              <h3>This portfolio you're looking at right now built with React and TypeScript to showcase my work and skills.</h3>
-
+              <h1>Maandag</h1>
+              <h2>08:30 tot 17:00</h2>
+              <h3>lokaal Groen 2073</h3>
+              <h4>Vak Flex</h4>
             </div>
           </div>
 
-
-
           <div className="card second">
-            <h1>Coming soon</h1>
-            <h2>Project</h2>
-            <h3>Building something new. Check back soon for more projects as I keep creating and coding cool things.</h3>
-
+            <h1>Dinsdag</h1>
+            <h2>08:30 tot 17:00</h2>
+            <h3>lokaal Groen 2073</h3>
+            <h4>Vak Flex</h4>
           </div>
         </div>
 
-
-
         <div className="card third">
-          <h1>Digital Arts Series</h1>
-          <h2>Creative</h2>
-          <h3>A collection of digital paintings character designs, concept art, and experiments mixing realism with cartoon aesthetics.</h3>
-
+          <h1>Woensdag</h1>
+          <h2>08:30 tot 17:00</h2>
+          <h3>lokaal Groen 2073</h3>
+          <h4>Vak Flex</h4>
         </div>
+      </div>
+
+      <div className="card fourth">
+        <h1>Donderdag</h1>
+        <h2>08:30 tot 17:00</h2>
+        <h3>lokaal Groen 2073</h3>
+        <h4>Vak Flex</h4>
+      </div>
+
+
+      <div className="card fifth">
+        <h1>Vrijdag</h1>
+        <h2>08:30 tot 13:30</h2>
+        <h3>lokaal Groen 2073</h3>
+        <h4>Vak Flex</h4>
       </div>
 
 
