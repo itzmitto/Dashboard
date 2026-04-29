14f370cce472dd0089be8bc99fdeffb3a4e29f37
Andre <andre.babirian2004@gmail.com>
2026-04-29 23:07:13 +0200

    19 commit

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from 'react-router-dom'
 
 export function HomeContent() {
   return (
-    <>
+    <> 
       <h1>Dit is het begin</h1>
       <p>Welkom op je mijn school dashboard</p>
     </>
@@ -18,7 +18,6 @@ function Sidebar() {
         <li><Link to="/main">Home</Link></li>
         <li><Link to="/main/rooster">Rooster</Link></li>
         <li><Link to="/main/taken">Taken</Link></li>
-        <li><Link to="/main/cijfers">Cijfers</Link></li>
         <li><Link to="/main/profiel">Profiel</Link></li>
         <li><Link to="/main/settings">Settings</Link></li>
       </ul>
