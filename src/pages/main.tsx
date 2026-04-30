41678de00f0bf965dba13f6777437872630de310
Andre <andre.babirian2004@gmail.com>
2026-04-30 22:11:11 +0200

    29

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,10 +3,31 @@ import { Link, Outlet } from 'react-router-dom'
 
 export function HomeContent() {
   return (
-    <> 
-      <h1>Dit is het begin</h1>
-      <p>Welkom op je mijn school dashboard</p>
-    </>
+     <div className="home-page">
+      <div className="gridContainer">
+        <div className="titleBlock">
+          <h1>School Rooster</h1>
+          <h2>Dit is mijn schoolrooster voor de week</h2>
+        </div>
+
+        <div className="cardsContainer">
+          <div className="card">
+            <h1>Maandag</h1>
+            <h2>08:30 tot 17:00</h2>
+            <h3>lokaal Groen 2.073</h3>
+            <h4>Vak: Flex</h4>
+          </div>
+
+          <div className="card">
+            <h1>Dinsdag</h1>
+            <h2>08:30 tot 17:00</h2>
+            <h3>lokaal Groen 2.073</h3>
+            <h4>Vak: Flex</h4>
+          </div>
+</div>
+</div>
+</div>
+
   )
 }
 
