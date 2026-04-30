695d7d022ec60a181f6b5ab7146b91960bab9f68
Andre <andre.babirian2004@gmail.com>
2026-04-30 22:55:10 +0200

    32 profiel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Main, { HomeContent } from './pages/main'
 import Rooster from './pages/rooster'
+import Profiel from './pages/profiel'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/main" element={<Main />}>
           <Route index element={<HomeContent />} />
           <Route path="rooster" element={<Rooster />} />
+          <Route path="profiel" element={<Profiel />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
