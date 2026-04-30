996b1c826dfe1602c96922643a32e0581ba4a1ef
Andre <andre.babirian2004@gmail.com>
2026-04-30 23:04:45 +0200

    33

--- src/pages/profiel.tsx
+++ src/pages/profiel.tsx
@@ -2,29 +2,18 @@ import '../styling/profiel.css';
 
 function Profiel() {
     return (
-        <div className="rooster-page">
+        <div className="profiel-page">
             <div className="gridContainer">
                 <div className="titleBlock">
-                    <h1>School Rooster</h1>
-                    <h2>Dit is mijn schoolrooster voor de week</h2>
+                    <h1>Profiel</h1>
+                    <h2>Dit is mijn profiel pagina</h2>
                 </div>
 
                 <div className="cardsContainer">
                     <div className="card">
-                        <h1>Maandag</h1>
-                        <h2>08:30 tot 17:00</h2>
-                        <h3>lokaal Groen 2.073</h3>
-                        <h4>Vak: Flex</h4>
+                        <h1>Naam</h1>
+                        <h2>Jouw naam hier</h2>
                     </div>
-
-                    <div className="card">
-                        <h1>Dinsdag</h1>
-                        <h2>08:30 tot 17:00</h2>
-                        <h3>lokaal Groen 2.073</h3>
-                        <h4>Vak: Flex</h4>
-                    </div>
-
-
                 </div>
             </div>
         </div>
