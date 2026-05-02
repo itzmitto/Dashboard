8393c06ef1e261f4f08d7c518e05f631b822ba12
Andre <andre.babirian2004@gmail.com>
2026-05-02 19:01:58 +0200

    52

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -5,8 +5,8 @@ function Settings() {
         <div className="rooster-page">
             <div className="gridContainer">
                 <div className="titleBlock">
-                    <h1>taken pagina</h1>
-                    <h2>Dit zijn mijn taken voor de week</h2>
+                    <h1>settings</h1>
+                    <h2>dit is de settings pagina </h2>
                 </div>
             </div>
         </div>
