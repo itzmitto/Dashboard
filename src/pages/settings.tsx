481c25d17b967086c5df06166252cc9f67828136
Andre <andre.babirian2004@gmail.com>
2026-05-02 19:00:53 +0200

    49 settings pagina aangemaakt en routes geconnect sidebar enz

---
+++ src/pages/settings.tsx
@@ -0,0 +1,16 @@
+import '../styling/settings.css';
+
+function Settings() {
+    return (
+        <div className="rooster-page">
+            <div className="gridContainer">
+                <div className="titleBlock">
+                    <h1>taken pagina</h1>
+                    <h2>Dit zijn mijn taken voor de week</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
