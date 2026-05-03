0a4fc375a61b2e702cc99d821c4c46ac15bdf41e
Andre <andre.babirian2004@gmail.com>
2026-05-03 21:29:42 +0200

    60

--- src/pages/leraren.tsx
+++ src/pages/leraren.tsx
@@ -11,27 +11,32 @@ function Leraren() {
                 <div className="cardsContainer">
                     <div className="card1">
                         <h1>Peter</h1>
-                        <h2>leraren</h2>
+                        <h2>vakken:</h2>
+                        <p>flex</p>
                         <h3>leraren</h3>
                     </div>
                     <div className="card2">
                         <h1>Jeroen</h1>
-                        <h2>leraren</h2>
+                        <h2>vakken:</h2>
+                        <p>flex</p>
                         <h3>leraren</h3>
                     </div>
                     <div className="card3">
                         <h1>Jan</h1>
-                        <h2>leraren</h2>
+                        <h2>vakken:</h2>
+                        <p>flex</p>
                         <h3>leraren</h3>
                     </div>
                     <div className="card4">
                         <h1>Alex</h1>
-                        <h2>leraren</h2>
+                        <h2>vakken:</h2>
+                        <p>rekenen</p>
                         <h3>leraren</h3>
                     </div>
                     <div className="card5">
                         <h1>?</h1>
-                        <h2>leraren</h2>
+                        <h2>vakken:</h2>
+                        <p>Nederlands</p>
                         <h3>leraren</h3>
                     </div>
                 </div>
